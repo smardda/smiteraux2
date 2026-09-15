ee183707e34b2a453570cfdcc9e8e2b9f8de5d29
Wayne Arter <wayne.arter@ukaea.uk>
2026-09-15 16:27:49 +0100

    added tar file creation

--- tests.bash
+++ tests.bash
@@ -6,3 +6,6 @@ for i in DEMO-double-null ITER-valid JET-ant JET-radn;do
 echo "******Start of Case name "$i
 (cd $i;./case.bash)
 done
+if [ $? -ne 0 ] ; then exit 1 ;fi
+#  Create tar file containing power deposition results
+tar cvf smiteraux2.tar $(find . -name "*_powx.vtk")
